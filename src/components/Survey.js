5cc411c244092471dafb75b61a37b5d9e55a3d29
Travis Brewer <t.brewer.co@gmail.com>
2021-07-19 12:36:12 -0600

    wip: add toggleForm function, commented out

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -2,6 +2,11 @@ import { useState } from 'react';
 import Form from './Form';
 
 function Survey(props) { // { survey, handleDelete, handleUpdate}
+
+    // function toggleForm() {
+    //     setEditFormVisible(!editFormVisible)
+    // }
+
     const [editFormVisible, setEditFormVisible] = useState(false)
     return (
         <div className="survey">
