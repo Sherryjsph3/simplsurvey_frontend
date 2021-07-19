943f3c9c574d6cd0c79c77ef6372c22e3360654d
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2021-07-19 11:00:45 -0400

    imports nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //=========COMPONENT IMPORTS=========
 import Header from './components/Header'
+import Nav from './components/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
     </div>
   );
 }
