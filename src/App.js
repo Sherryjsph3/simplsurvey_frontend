5a4281480aa4612534b53d2e447149ca939e5583
Caleb Confer <calebconfer@Calebs-MacBook-Air.local>
2021-07-19 15:18:12 -0400

    adds fetch request for survey data

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@
 import Header from './components/Header'
 import Nav from './components/Nav'
 import Main from './components/Main'
+//===========CSS IMPORT===============
 import './App.css';
+// ==========HOOKS====================
+import {useState, useEffect} from "react"
+
 
 function App() {
+  const [surveyState, setSurveyState] = useState({
+    surveys: []});
+
+  useEffect(() => {
+    async function getSurveys() {
+      try {
+        const surveys = await fetch('http://localhost:3000/surveys').then(response => response.json()) 
+        setSurveyState({surveys})
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getSurveys();
+  }, []);
+
   return (
     <div className="App">
       <Header />
