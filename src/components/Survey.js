b5e6a2c2292ac8585ed90ebe73fe5476a011bd8e
Travis Brewer <t.brewer.co@gmail.com>
2021-07-21 10:53:56 -0600

    wip: intialize surveyFormState, add handleSubmitAnswer function

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+
 function Survey({ survey }) {
 
     // function toggleForm() {
@@ -5,22 +8,37 @@ function Survey({ survey }) {
     // }
 
     // const [editFormVisible, setEditFormVisible] = useState(false)
+    const [surveyFormState, setSurveyFormState] = useState({
+        buttonText: '',
+    })
+
+    function handleSubmitAnswer(event) {
+        event.preventDefault();
+        console.log("SUBMITTED ANSWER");
+    }
 
     return (
         <div className="survey">
             <h3>{survey.survey_question_text}</h3>
             <div className="pollSelectionArea">
                 <div className="selectYourOptionText">Select your choice:</div>
-                <form class="pollForm">
+                <form onSubmit={handleSubmitAnswer} className="surveyForm">
                     <div>
                         {survey.answer_options.map(option => {
                             return (
                                 <>
-                                    <button onClick="vote"
-                                        className="pollButton" default-value={option.answer_text}
-                                        alt-value="AFTER CLICK">
-                                        <span className="GraphicHere"></span>
-                                        <span className="buttonText">{option.answer_text}</span>
+                                    <button
+                                        key={option.answer_option_id}
+                                        type="submit"
+                                        className="pollButton"
+                                        textDefaultValue={option.answer_text}
+                                        textAltValue="AFTER CLICK"
+                                        value={option.id}
+                                    >
+                                        <span className="GraphicOrIconHere">Graphic or Icon?
+                                        </span>
+                                        <span className="buttonText">{option.answer_text}
+                                        </span>
                                     </button> <br />
                                 </>
                             )
@@ -28,7 +46,7 @@ function Survey({ survey }) {
                     </div>
                 </form>
             </div>
-        </div>
+        </div >
     )
 }
 
