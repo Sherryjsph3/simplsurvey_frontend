987867da0904641abf824da31003418c65b5df1b
Travis Brewer <t.brewer.co@gmail.com>
2021-07-23 04:58:04 -0600

    wip: incrementor working, votes displaying in button

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,36 +1,44 @@
+import { useState } from "react";
+
+
 function Survey({ survey }) {
 
-    // function toggleForm() {
-    //     setEditFormVisible(!editFormVisible)
-    // }
+    function getRandom(maxNum) {
+        return Math.floor(Math.random() * 50);
+    }
+    const [votes, setVotes] = useState((getRandom()));
 
-    // const [editFormVisible, setEditFormVisible] = useState(false)
+    function handleIncrement() {
+        setVotes(votes + 1)
+    }
+
 
     return (
-      
+
         <div className="survey">
             <h3 className='ques-h3'>{survey.survey_question_text}</h3>
             <div className="pollSelectionArea">
                 <div className="selectYourOptionText">Select your choice:</div>
-                <form class="pollForm">
-                    <div>
-                        {survey.answer_options.map(option => {
-                            return (
-                                <>
-                                    <button onClick="vote"
-                                        className="pollButton" default-value={option.answer_text}
-                                        alt-value="AFTER CLICK">
-                                        <span className="GraphicHere"></span>
-                                        <span className="buttonText">{option.answer_text}</span>
-                                    </button> <br />
-                                </>
-                            )
-                        })}
-                    </div>
-                </form>
+                <div>
+                    {survey.answer_options.map(option => {
+                        return (
+                            <>
+                                <button
+                                    onClick={handleIncrement}
+                                    className="pollButton" default-value={option.answer_text}
+                                    alt-value="AFTER CLICK"
+                                    key={option.id}>
+                                    <span className="GraphicHere"></span>
+                                    <span className="buttonText">{option.answer_text}</span>
+                                    <span className="voteCount">{votes}</span>
+                                </button> <br />
+                            </>
+                        )
+                    })}
+                </div>
             </div>
         </div>
-      
+
     )
 }
 
